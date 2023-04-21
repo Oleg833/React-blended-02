73b464c5c569a3c32afdc1c34c5277d18ca88cab
sps-pashka-it <stetsyuk.p.s@gmail.com>
2023-04-21 21:45:48 +0300

    vitaliy

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -5,32 +5,37 @@ import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
 export class SearchForm extends Component {
   state = {
-  query: "",
-  }
-  handleInput =(event) => {
+    query: '',
+  };
+  handleInput = event => {
     // console.log(event.target.value);
     // console.log(this);
     this.setState({ query: event.target.value });
-  }
+  };
 
-  handleOnSubmit = (event) => {
+  handleOnSubmit = event => {
     event.preventDefault();
+    if (!this.state.query) {
+      alert('Please enter a query');
+      return;
+    }
     this.props.onSubmit(this.state.query);
-    this.setState({ query: "" });
-  }
+    this.setState({ query: '' });
+  };
   render() {
     return (
       <SearchFormStyled onSubmit={this.handleOnSubmit}>
-  <FormBtn type="submit">
-    <FiSearch size="16px" />
-  </FormBtn>
-  <InputSearch
-    onChange ={this.handleInput} 
-    placeholder="What do you want to write?"
-    name="search"
-    required
-    autoFocus
-  />
-</SearchFormStyled>);
+        <FormBtn type="submit">
+          <FiSearch size="16px" />
+        </FormBtn>
+        <InputSearch
+          onChange={this.handleInput}
+          placeholder="What do you want to write?"
+          name="search"
+          required
+          autoFocus
+        />
+      </SearchFormStyled>
+    );
   }
 }
